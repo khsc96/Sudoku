79f06819c2c892fa5edce87e7192a050a489fa48
Kenny Ho <50527804+khsc96@users.noreply.github.com>
2020-07-08 19:04:18 +0800

    Backbone code of sudoku

---
+++ Sudoku.py
@@ -0,0 +1,44 @@
+# Sudoku game and solver using python
+# solver uses backtracking algorithm
+
+import pprint
+
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]]
+
+# Make a new game with new board
+def generate():
+    pass
+
+# Solve the game
+def solver(board):
+    pass
+
+def find_empty_slot(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+
+def print_board(board): 
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - ")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+print_board(board)
