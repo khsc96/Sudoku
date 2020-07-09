7fbd14ff968a3949262875db734df333d4d9ca02
khsc96 <khsc96@gmail.com>
2020-07-09 16:21:16 +0800

    Complete Sudoku

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Making Sudoku puzzle with python
 1. Learn backtracking algorithm
 2. Familiarse myself with Python
 
+## Acknowledgements
+1. [Tech with Tim YouTube tutorial](https://www.youtube.com/watch?v=eqUwSA0xI-s&list=PLzMcBGfZo4-kE3aF6Y0wNBNih7hWRAU2o)
--- Sudoku.py
+++ Sudoku.py
@@ -15,18 +15,61 @@ board = [
     [0, 4, 9, 2, 0, 6, 0, 0, 7]]
 
 # Make a new game with new board
-def generate():
+def generate_boarad():
     pass
 
 # Solve the game
 def solver(board):
-    pass
+    empty_slot = find_empty_slot(board)
+
+    if not empty_slot:
+        # Finish solving Sudoku
+        return True
+
+    row, col = empty_slot
+
+    for i in range(1, 10):
+        if valid_input(board, i, empty_slot):
+            print(i)
+            print(empty_slot)
+            board[row][col] = i
+            print("--- one iteration ---")
+            print_board(board)
+
+            if solver(board):
+                return True
+            
+            board[row][col] = 0
+
+    return False
+
 
 def find_empty_slot(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return (i, j)
+
+    return None
+
+def valid_input(board, input, pos):
+    row_pos = pos[0]
+    col_pos = pos[1]
+
+    for i in range(len(board[0])):
+        if col_pos != i and board[row_pos][i] == input:
+            return False   
+        if row_pos != i and board[i][col_pos] == input:
+            return False    
+
+    box_row_pos = pos[0] // 3
+    box_col_pos = pos[1] // 3
+
+    for i in range(box_row_pos * 3, box_row_pos * 3 + 3):
+        for j in range(box_col_pos * 3, box_col_pos * 3 + 3):
+            if input == board[i][j] and (i, j) != pos:
+                return False
+    return True
 
 def print_board(board): 
     for i in range(len(board)):
@@ -41,4 +84,7 @@ def print_board(board):
             else:
                 print(str(board[i][j]) + " ", end="")
 
+print_board(board)
+print("------------------------")
+solver(board)
 print_board(board)
